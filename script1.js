b2ca2786102391f1c4be24f91fef7e0bfd898dd1
Swastik Poojary <145894869+swift-9@users.noreply.github.com>
2023-12-03 19:54:49 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -10,7 +10,7 @@ optionImages.forEach((image, index) => {
   image.addEventListener("click", (e) => {
     image.classList.add("active");
 
-    userResult.src = cpuResult.src = "images/rock.png";
+    userResult.src = cpuResult.src = "rock.png";
     result.textContent = "Wait...";
 
     // Loop through each option image again
@@ -34,7 +34,7 @@ optionImages.forEach((image, index) => {
       // Generate a random number between 0 and 2
       let randomNumber = Math.floor(Math.random() * 3);
       // Create an array of CPU image options
-      let cpuImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
+      let cpuImages = ["rock.png", "paper.png", "scissors.png"];
       // Set the CPU image to a random option from the array
       cpuResult.src = cpuImages[randomNumber];
 
